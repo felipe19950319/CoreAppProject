83d548768a1282a9e00b930156e8c0d1f14e387f
felipe.pasache <felipe_pr19@outlook.com>
2018-05-04 17:44:23 -0300

    Conexion my sql y creacion de retornos con transaccion dinamica

--- WorkScheme/ConsoleTest/Program.cs
+++ WorkScheme/ConsoleTest/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using SqlConnector;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ConsoleTest
 {
@@ -10,6 +13,36 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
+
+            string connectionString="";
+            connectionString = 
+                "SERVER=" + "localhost" + ";"
+                +"PORT=" +"3309"+";"
+                + "DATABASE=" +"test" + ";"
+                + "UID=" + "root" + ";"
+                + "PASSWORD=" + "19047321k" + ";" +
+                "SslMode=none;";
+
+
+
+
+
+            MySqlConnector m = new MySqlConnector();
+            m.ConnectionString = connectionString;
+            m.AddProcedure("new_procedure");
+              m.
+                  AddParameter("", "").
+                  AddParameter("", "").
+                  AddParameter("", "").
+                  AddParameter("", "");
+            DataTable dt = new DataTable();
+            /*resultado a datatable*/
+            dt = m.ExecQuery().toDataTable();
+            /*XDocument xdoc = new XDocument();
+            xdoc = m.ExecQuery().toXml();*/
+
+
+            Console.ReadKey();
         }
     }
 }
--- WorkScheme/SqlConnector/Common.cs
+++ WorkScheme/SqlConnector/Common.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace SqlConnector
 {
-    class Common
+    public class Common
     {
+        public Dictionary<string, string> Parameters = new Dictionary<string, string>();
+        public string Procedure = string.Empty;
+
+        public void AddProcedure(string Procedure)
+        {
+            this.Procedure = Procedure;
+        }
+
+        public Common AddParameter(string Parameter,string Value)
+        {
+            Parameters.Add(Parameter, Value);
+            return this;
+        }
     }
+
+
 }
